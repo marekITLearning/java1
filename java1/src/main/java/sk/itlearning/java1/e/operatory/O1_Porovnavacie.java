19a3444be8c2c722fab33ec94a3a3921381ece45
student <student@U509PL.itl.lan>
2022-09-28 10:43:30 +0200

    updated equals in Firma

--- java1/src/main/java/sk/itlearning/java1/e/operatory/O1_Porovnavacie.java
+++ java1/src/main/java/sk/itlearning/java1/e/operatory/O1_Porovnavacie.java
@@ -1,5 +1,7 @@
 package sk.itlearning.java1.e.operatory;
 
+import sk.itlearning.java1.g.zakaznik.Firma;
+
 public class O1_Porovnavacie {
 
 	// == != > >= < <=
@@ -13,6 +15,16 @@ public class O1_Porovnavacie {
 		System.out.println(a != b);
 		System.out.println(a > b);
 		System.out.println(a >= b);
+		
+		String s1 = "text";
+		String s2 = new String("text");
+		
+		System.out.println(s1.equals(s2));
+		
+		Firma f1 = new Firma("11", "f");
+		Firma f2 = new Firma("11", "f");
+		
+		System.out.println(f1.equals(f2));
 
 	}
 
--- java1/src/main/java/sk/itlearning/java1/g/zakaznik/Firma.java
+++ java1/src/main/java/sk/itlearning/java1/g/zakaznik/Firma.java
@@ -1,11 +1,13 @@
 package sk.itlearning.java1.g.zakaznik;
 
+import java.util.Objects;
+
 public class Firma implements Zakaznik {
 
 	private String ico;
-	
+
 	private String nazov;
-	
+
 	public Firma(String ico, String nazov) {
 		setIco(ico);
 		setNazov(nazov);
@@ -26,10 +28,25 @@ public class Firma implements Zakaznik {
 	public void setNazov(String nazov) {
 		this.nazov = nazov;
 	}
-	
+
 	@Override
 	public String getId() {
 		return getIco();
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Firma) {
+			Firma f2 = (Firma) obj;
+			return Objects.equals(this.getIco(), f2.getIco()) 
+					&& Objects.equals(this.getNazov(), f2.getNazov());
+		}
+		return false;
+	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.getIco(), this.getNazov());
+	}
+
 }
